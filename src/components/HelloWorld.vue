239bad2679eae08ecb3692c600706c3e279bc4a3
Jimmy Ou <jimmy.ou@oocl.com>
2019-07-19 15:28:58 +0800

    finish

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <button @click="decrease">
+      -
+    </button>
+    <div>
+      {{ count }}
+    </div>
+    <button @click="increase">
+      +
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increase() {
+      this.count++;
+    },
+    decrease() {
+      this.count--;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
